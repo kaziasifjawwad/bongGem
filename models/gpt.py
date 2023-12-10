54b737f07ba7239e719a1e9ac7b9ca42f9a30d5e
kaziasifjawwad <kaziasifjawwad@gmail.com>
2023-12-10 21:16:39 +0600

    refactored some packages name

--- models/gpt.py
+++ models/gpt.py
@@ -2,7 +2,7 @@ from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
 from transformers import Trainer, TrainingArguments
 
-from models.Model import ParentModel
+from models.model import ParentModel
 
 
 class BengaliGpt(ParentModel):
